3cf44d1396e475d55177b5fdbb30fbabb8a92622
Muhammed Iqbal <iquzart@hotmail.com>
2025-03-22 15:42:48 +0400

    [fix] kubernetes auth

--- scripts/install_hashicorp_vault.sh
+++ scripts/install_hashicorp_vault.sh
@@ -3,6 +3,8 @@ function install_hashicorp_vault() {
   helm upgrade -i kind-vault -n vault --create-namespace \
   --atomic -f apps/hashicorp-vault/deploy/values-kind-vault.yaml apps/hashicorp-vault/deploy/vault-helm-0.22.1
 
+  mkdir -p apps/hashicorp-vault/CaC/vault/kubernetes/kind_cluster
+
   kubectl config view --raw -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' | base64 --decode > apps/hashicorp-vault/CaC/vault/kubernetes/kind_cluster/ca.pem
 
   kubectl get secret kind-vault -n vault -o jsonpath='{.data.token}' |  base64 --decode > apps/hashicorp-vault/CaC/vault/kubernetes/kind_cluster/token_reviewr_jwt
