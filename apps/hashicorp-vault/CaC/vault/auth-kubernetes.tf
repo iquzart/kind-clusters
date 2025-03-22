afa69a1317b4d1adcef4764d8a5224905d7ab7e5
Muhammed Iqbal <iquzart@hotmail.com>
2025-03-22 18:23:04 +0400

    [vault] enhancements

--- apps/hashicorp-vault/CaC/vault/auth-kubernetes.tf
+++ apps/hashicorp-vault/CaC/vault/auth-kubernetes.tf
@@ -2,22 +2,23 @@
 # Enable Kubernetes Auth Method
 #----------------------------------------------------------
 
-# kind_cluster
-resource "vault_auth_backend" "kind_cluster" {
+# devops_cluster
+resource "vault_auth_backend" "devops_cluster" {
  type = "kubernetes"
- path = "kubernetes/kind_cluster"
+ path = "kubernetes/devops_cluster"
 }
 
-resource "vault_kubernetes_auth_backend_config" "kind_cluster" {
- backend                = vault_auth_backend.kind_cluster.path
+resource "vault_kubernetes_auth_backend_config" "devops_cluster" {
+ backend                = vault_auth_backend.devops_cluster.path
  kubernetes_host        = var.kubernetes_host
- kubernetes_ca_cert     = file("kubernetes/kind_cluster/ca.pem")
- token_reviewer_jwt     = file("kubernetes/kind_cluster/token_reviewr_jwt")
+ kubernetes_ca_cert     = file("kubernetes/devops_cluster/ca.pem")
+ token_reviewer_jwt     = file("kubernetes/devops_cluster/token_reviewr_jwt")
  issuer                 = "https://kubernetes.default.svc.cluster.local"
+ disable_iss_validation = "true"
 }
 
-resource "vault_kubernetes_auth_backend_role" "kind_cluster_role_go_app" {
- backend                          = vault_auth_backend.kind_cluster.path
+resource "vault_kubernetes_auth_backend_role" "devops_cluster_role_go_app" {
+ backend                          = vault_auth_backend.devops_cluster.path
  role_name                        = "go-app"
  bound_service_account_names      = ["*"]
  bound_service_account_namespaces = ["*"]
